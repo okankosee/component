b1007f3ffce888af8304a5d58b2104e49bfb2f8f
okan köse <okan.kosee@outlook.com>
2024-01-11 10:59:04 +0300

    initial commit

---
+++ assets/sales.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Sales() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="14"
+      fill="none"
+      viewBox="0 0 15 14"
+    >
+      <g clipPath="url(#clip0_1_49)">
+        <path
+          fill="#737373"
+          d="M7.375 9.96a.159.159 0 00.195.045.159.159 0 00.056-.045l2.23-2.815a.158.158 0 00-.125-.256H8.255V.166a.16.16 0 00-.16-.159H6.9a.16.16 0 00-.159.16v6.72H5.269a.158.158 0 00-.126.256L7.375 9.96zm7.416-.695h-1.195a.16.16 0 00-.16.159v3.059H1.564v-3.06a.16.16 0 00-.16-.158H.21a.16.16 0 00-.16.159v3.933c0 .351.285.636.638.636h13.626a.636.636 0 00.638-.636V9.424a.16.16 0 00-.16-.16z"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_1_49">
+          <path fill="#fff" d="M0 0H15V14H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default Sales;
